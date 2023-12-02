17b2656472ae0a5ae142d91993e864c3fd54c4fd
cassidybeni <cassidybeni@pursuit.org>
2023-12-01 20:41:30 -0500

    Debug cors issue

--- back-end/app.js
+++ back-end/app.js
@@ -1,23 +1,22 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
-const bookedController = require("./Controllers/bookedcontroller.js")
-const checklistController = require("./Controllers/checklistcontroller.js")
-const eventsController = require("./Controllers/eventController.js")
-const favoritesController = require("./Controllers/favoritescontroller.js")
-const usersController = require("./Controllers/usersController")
-
+const bookedController = require("./Controllers/bookedcontroller.js");
+const checklistController = require("./Controllers/checklistcontroller.js");
+const eventsController = require("./Controllers/eventController.js");
+const favoritesController = require("./Controllers/favoritescontroller.js");
+const usersController = require("./Controllers/usersController");
 
 // CONFIGURATION
 const app = express();
 
+
 app.use(cors({
   origin: "https://eventful-capstone.netlify.app",
-  methods: 'GET,POST,UPDATE,DELETE',
+  methods: 'GET, POST, PUT, DELETE',
 }));
 
 // MIDDLEWARE
-app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
@@ -25,21 +24,15 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use("/events", eventsController)
+app.use("/events", eventsController);
+app.use("/checklist", checklistController);
+app.use("/favorites", favoritesController);
+app.use("/booked", bookedController);
+app.use("/users", usersController);
 
-app.use("/checklist", checklistController)
-
-app.use("/favorites", favoritesController)
-
-app.use("/booked", bookedController)
-
-app.use("/users", usersController)
-
-
-app.get("*", (req,res)=>{
-  res.send("page not found")
-})
-
+app.get("*", (req, res) => {
+  res.send("page not found");
+});
 
 // EXPORT
 module.exports = app;

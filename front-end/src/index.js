58110c0b926528b26dac1bd3896117ce5ee47ca0
cassidybeni <cassidybeni@pursuit.org>
2023-12-08 12:00:22 -0500

    Update to create root

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { UserProvider } from "./Providers/UserProvider";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
-
-reportWebVitals();

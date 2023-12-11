bf8c8abaf3b263ab2b027dfd2af890552095fa45
cassidybeni <cassidybeni@pursuit.org>
2023-12-10 20:58:45 -0500

    Fix route

--- front-end/src/Components/PrivateRoute.js
+++ front-end/src/Components/PrivateRoute.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { UserContext } from "../Providers/UserProvider";
 
 const PrivateRoute = ({ path, element }) => {
   const currentUser = useContext(UserContext);
 
   return (
-    <Route
-      path={path}
-      element={currentUser.currentUser ? element : <Navigate to="/signin" />}
-    />
+    <Routes>
+      <Route
+        path={path}
+        element={currentUser.currentUser ? element : <Navigate to="/signin" />}
+      />
+    </Routes>
   );
 };
 

9b231529b364bf6060df1c9978614d5c6dc12054
cassidybeni <cassidybeni@pursuit.org>
2024-01-13 17:56:36 -0500

    Debugging

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const axios = require("axios");
 const bookedController = require("./Controllers/bookedcontroller.js");
 const checklistController = require("./Controllers/checklistcontroller.js");
 const eventsController = require("./Controllers/eventController.js");
@@ -11,12 +12,11 @@ const usersController = require("./Controllers/usersController");
 const app = express();
 const corsOptions = {
   origin: "https://event-ful.adaptable.app",
-  optionsSuccessStatus: 200,
+  credentials: true,
 };
 
-app.use(cors());
-
 // MIDDLEWARE
+app.use(cors(corsOptions));
 app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
@@ -24,26 +24,27 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use(cors(corsOptions));
 app.use("/events", eventsController);
 app.use("/checklist", checklistController);
 app.use("/favorites", favoritesController);
 app.use("/booked", bookedController);
 app.use("/users", usersController);
 
-app.get('/proxy-ipstack', async (req, res) => {
+app.get("/proxy-ipstack", async (req, res) => {
   const { ip, accessKey } = req.query;
 
   try {
-      const response = await axios.get(`http://api.ipstack.com/${ip}?access_key=${accessKey}`);
-      res.json(response.data);
+    const response = await axios.get(
+      `http://api.ipstack.com/${ip}?access_key=${accessKey}`
+    );
+    res.json(response.data);
   } catch (error) {
-      res.status(error.response?.status || 500).json({ error: error.message });
+    res.status(error.response?.status || 500).json({ error: error.message });
   }
 });
 
 app.get("*", (req, res) => {
-  res.send("page not found");
+  res.send("Page not found");
 });
 
 // EXPORT
